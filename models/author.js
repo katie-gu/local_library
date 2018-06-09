286dae2a3b88e5beef8e711afb99da00255122ba
katie-gu <katiegu555@gmail.com>
2018-06-08 19:35:42 -0700

    First version of application released.

---
+++ models/author.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+var moment = require('moment');
+var Schema = mongoose.Schema;
+
+var AuthorSchema = new Schema({
+    first_name: {type: String, required: true, max: 100},
+    family_name: {type: String, required: true, max: 100},
+    date_of_birth: {type: Date},
+    date_of_death: {type: Date}
+});
+
+AuthorSchema.virtual('name').get(function () {
+    return this.family_name + ',' + this.first_name;
+});
+
+AuthorSchema.virtual('url').get(function () {
+    return '/catalog/author/' + this._id;
+});
+
+AuthorSchema.virtual('date_of_birth_formatted').get(function(){
+  return moment(this.date_of_birth).format('MM-DD-YYYY');
+});
+
+AuthorSchema.virtual('date_of_death_formatted').get(function(){
+  return this.date_of_death ? moment(this.date_of_death).format('MM-DD-YYYY') : '';
+});
+module.exports = mongoose.model('Author', AuthorSchema);
